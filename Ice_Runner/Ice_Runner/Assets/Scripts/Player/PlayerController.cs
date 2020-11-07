15629e1cff3bedf30fa5307a969c35a3acc140c7
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2020-11-07 01:19:14 +0100

    Wall Jump

--- Ice_Runner/Ice_Runner/Assets/Scripts/Player/PlayerController.cs
+++ Ice_Runner/Ice_Runner/Assets/Scripts/Player/PlayerController.cs
@@ -11,7 +11,11 @@ public class PlayerController : MonoBehaviour
     private bool isJumping = false;
     private float jumpTime = 0.35f;
     private float jumpTimeCounter;
+    private bool wallJumping = false;
+    private float wallJumpForce = 4.0f;
+    private Vector2 wallJumpDirection = new Vector2(-1.0f, 1.5f);
     public LayerMask floorLayer;
+    public LayerMask wallsLayer;
 
     //Components
     private Animator anim;
@@ -32,6 +36,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        //Jumping
         if (input.basicJump && isTouchingFloor())//Cuando se presione espacio se salta
         {
             isJumping = true;
@@ -46,17 +51,33 @@ public class PlayerController : MonoBehaviour
         {
             isJumping = false;
         }
+
+        //Wall-jumping
+        if (!isTouchingFloor() && isTouchingWall())
+        {
+            if (input.basicJump)
+            {
+                WallJump();
+            }
+        }
+        if (isTouchingFloor())
+        {
+            wallJumping = false;
+            wallJumpDirection.x = -1;
+        }
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(xSpeed, rb.velocity.y);
+        if (!wallJumping)
+            rb.velocity = new Vector2(xSpeed, rb.velocity.y);
     }
 
     private void Jump()
     {
         rb.AddForce(Vector2.up * jumpForceBasic, ForceMode2D.Impulse);
     }
+
     private void JumpIntensity()
     {
         if (jumpTimeCounter > 0)
@@ -70,8 +91,20 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void WallJump()
+    {
+        wallJumping = true;
+        rb.AddForce(wallJumpDirection * wallJumpForce, ForceMode2D.Impulse);
+        wallJumpDirection.x *= -1; 
+    }
+
     private bool isTouchingFloor() //We cast a ray from the player's position to check if it touches the floor layer
     {
         return Physics2D.Raycast(this.transform.position, Vector2.down, 0.5f, floorLayer);
+    }
+
+    private bool isTouchingWall() //We cast a ray from the player's position to check if it touches the floor layer
+    {
+        return Physics2D.Raycast(this.transform.position, Vector2.left, 0.5f, wallsLayer) || Physics2D.Raycast(this.transform.position, Vector2.right, 0.5f, wallsLayer);
     }
 }
