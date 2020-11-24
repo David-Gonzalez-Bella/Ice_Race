4270617e36e7a0f8e5203eb1de288b066e60b7c9
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2020-11-24 17:14:11 +0100

    Enemies implemented

---
+++ Ice_Runner/Ice_Runner/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Health))]
+
+public class Enemy : MonoBehaviour
+{
+    //Variables
+    //Movement
+    protected int direction = 1;
+    public float xSpeed;
+    public float startMovePoint;
+    public float endMovePoint;
+
+    //References
+    protected Rigidbody2D rb;
+    protected SpriteRenderer sr;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Behaviour(); //All enemies will do its behaviour every frame
+    }
+
+    protected virtual void Behaviour() { } //Each enemy has a different behaviour
+}
